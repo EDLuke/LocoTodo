3ae94a937d445f81b3fe35a5bde5cf101541aae1
EDLuke <lukezhang213@gmail.com>
2017-04-12 22:52:41 -0400

    Added ParallaxLayerLayout, need to customize for more visible effects, good for now.

--- app/src/main/java/com/lukez/locotodo/MapsActivity.java
+++ app/src/main/java/com/lukez/locotodo/MapsActivity.java
@@ -2,6 +2,7 @@ package com.lukez.locotodo;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,15 +10,33 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.schibsted.spain.parallaxlayerlayout.ParallaxLayerLayout;
+import com.schibsted.spain.parallaxlayerlayout.SensorTranslationUpdater;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private SensorTranslationUpdater mSensorTranslationUpdater;
+    private ParallaxLayerLayout mParallax;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        //Set to full screen
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
+        //Link the sensor translation updater with the parallax layer
+        mParallax = (ParallaxLayerLayout)(this.findViewById(R.id.parallax));
+        mSensorTranslationUpdater = new SensorTranslationUpdater(this);
+        mParallax.setTranslationUpdater(mSensorTranslationUpdater);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -42,5 +61,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        //Re-register the sensor manager
+        mSensorTranslationUpdater.registerSensorManager();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+
+        //Un-register the sensor manager
+        mSensorTranslationUpdater.unregisterSensorManager();
     }
 }
